20643aa3b3106f802c8921dd8a2a451d33bfe501
Chris <christophschmid96@yahoo.de>
2022-06-24 17:22:04 +0200

    Final Commit, I've implemented the color picker incl. an eraser. On the right side is a Notepad for Ideas (just for symmetrical sake). Everything is as pretty as it is in my abilities.

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 const container = document.getElementById("grid");
 const resetBtn = document.getElementById("reset");
+const blackBtn = document.getElementById("black");
 const redBtn = document.getElementById("red");
 const blueBtn = document.getElementById("blue");
-const rainbowBtn = document.getElementById("rainbow");
-
-function colorPicker(){
-
-}
+const eraserBtn = document.getElementById("eraser");
 
 function createGrid(gridSize){
   for (i = 0; i < gridSize ** 2; i++) {
@@ -17,10 +14,19 @@ function createGrid(gridSize){
     container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
   }
+  let color = "black";
   const gridCells = document.querySelectorAll('.cells');
   gridCells.forEach(cell => cell.addEventListener("mouseover", function(){
-    cell.style.backgroundColor = "blue";
-  }))
+    if(blackBtn.checked == true){
+      cell.style.backgroundColor = "black";
+    }else if (blueBtn.checked == true){
+      cell.style.backgroundColor = "blue";
+    }else if (redBtn.checked == true){
+      cell.style.backgroundColor = "red";
+    }else if (eraserBtn.checked == true){
+      cell.style.backgroundColor = "white";
+    }
+  })) 
 }
 
 function resetGrid(){
